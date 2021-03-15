8e76d664dcdb72b64fc3955da726770a69e78d4d
Jackson Moreira <jacksonmoreiraneto@gmail.com>
2021-03-15 15:17:32 -0300

    treino003.py
    Consertei um erro

--- Scripts/treino003.py
+++ Scripts/treino003.py
@@ -20,4 +20,4 @@ if num2 < num1 and num2 < num3:
     print('O menor número é {}!'.format(num2))
 if num3 < num1 and num3 < num2:
     print('O menor número é {}!'.format(num3))
-print('--ENCERRANDO PROGRAMA--')
+print('--ENCERRANDO-PROGRAMA--')
