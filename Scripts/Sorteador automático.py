bb1a991e7915342700a2a687c1bf0bc44657c121
Jackson Moreira <jacksonmoreiraneto@gmail.com>
2021-03-16 14:14:10 -0300

    python
    Resgatado

---
+++ Scripts/Sorteador automático.py
@@ -0,0 +1,27 @@
+from random import randint
+print('#' * 30)
+print('     SORTEADOR AUTOMÁTICO     ')
+print('#' * 30)
+print('=' * 45)
+print('''Digite o que ou quem
+participará do sorteio, se for
+digitar um número, escreva-o por extenso!''')
+print('-' * 20)
+print('''AVISO: Número máximo de integrantes
+do sorteio: 5!''')
+print('-' * 20)
+mot = str(input('Digite para qual motivo o sorteio será realizado: ').lower())
+co1 = str(input('Digite a primeira coisa a ser sorteada: ').strip())
+co2 = str(input('Digite a segunda coisa a ser sorteada: ').strip())
+co3 = str(input('Digite a terceira coisa a ser sorteada: ').strip())
+co4 = str(input('Digite a quarta coisa a ser sorteada: ').strip())
+co5 = str(input('Digite a quinta coisa a ser sorteada: ').strip())
+lis = [co1, co2, co3, co4, co5]
+lis1 = lis[randint(0, len(lis))]
+print('O sorteado para {}, foi:'.format(mot))
+print('{}!'.format(lis1))
+print('-' * 20)
+print('''Quantos aos que não foram sorteados
+não desanimem. A próximo sorteio pode ser seu!''')
+print('--ENCERRANDO PROGRAMA--')
+print('=' * 45)
