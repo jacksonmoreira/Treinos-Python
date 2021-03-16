78976afe14c8f91daae0adbb2d539100f200de1f
Jackson Moreira <jacksonmoreiraneto@gmail.com>
2021-03-16 07:49:28 -0300

    treino001.py
    correção

--- Scripts/treino001.py
+++ Scripts/treino001.py
@@ -3,8 +3,8 @@ print('     MÉDIA ESCOLAR     ')
 print('-=-' * 30)
 print('''Digite duas notas e
 eu irei tirar a média entre elas.''')
-n1 = float(input('Digite a sua primeira nota: ').strip)
-n2 = float(input('Digite a sua segunda nota: ').strip)
+n1 = float(input('Digite a sua primeira nota: '))
+n2 = float(input('Digite a sua segunda nota: '))
 me = n1 + n2 / 2
 print('Sua primeira nota foi {} e sua segunda nota foi {}!'.format(n1, n2))
 if me <= 14:
